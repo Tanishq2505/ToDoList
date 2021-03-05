7d9304481a0e85fba5c86310754748a040064ffd
Tanishq Kashyap <tanishqa89@gmail.com>
2021-03-05 08:31:41 +0530

    pull to refresh added

--- app/src/main/java/com/devtk/todolist/HomepageActivity.kt
+++ app/src/main/java/com/devtk/todolist/HomepageActivity.kt
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
@@ -35,10 +36,12 @@ val firebaseAuthCUser = FirebaseAuth.getInstance().currentUser!!.uid
 private var onBackPressedToexit = false
 class HomepageActivity : AppCompatActivity() {
     val adapter = GroupAdapter<GroupieViewHolder>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
         findViewById<RecyclerView>(R.id.all_tasks_recycler).layoutManager = LinearLayoutManager(this)
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.refreshRecycler)
         val sharedPreferences = getSharedPreferences("sharedPrefs", AppCompatActivity.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         val mode = AppCompatDelegate.getDefaultNightMode()
@@ -82,11 +85,19 @@ class HomepageActivity : AppCompatActivity() {
         }
         val itemTouchHelper = ItemTouchHelper(simpleCallback)
         itemTouchHelper.attachToRecyclerView(findViewById(R.id.all_tasks_recycler))
+        swipeRefreshLayout.setOnRefreshListener {
+            adapter.notifyDataSetChanged()
+            swipeRefreshLayout.isRefreshing = false
+        }
 
     }
 
     val simpleCallback = object : SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
-        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
             return false
         }
         
@@ -97,68 +108,118 @@ class HomepageActivity : AppCompatActivity() {
                 val position = viewHolder.adapterPosition
                 adapter.removeGroupAtAdapterPosition(position)
 
-                database.child("Users").child(firebaseAuthCUser).child("Notes").addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        snapshot.children.forEach {
-                            val values = it.getValue(Notes::class.java)
-                            if (values != null) {
-                                if (values.task_name == noteName.toString()) {
-                                    val key = it.key
-                                    val deletedData = it.getValue(Notes::class.java)
-                                    database.child("Users").child(firebaseAuthCUser).child("Notes").child(key.toString()).removeValue()
-                                    Snackbar.make(findViewById(R.id.all_tasks_recycler), noteName, Snackbar.LENGTH_LONG).setAction("Undo", View.OnClickListener {
-                                        database.child("Users").child(firebaseAuthCUser).child("Notes").child(deletedData?.key.toString()).setValue(deletedData)
-                                        if (deletedData != null) {
-                                            adapter.add(position, tasks_viewholder(adapter, deletedData))
-                                        }
-                                    }).show()
-                                }
-                            }
-                        }
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {
-                        //TODO("Not yet implemented")
-                    }
-
-                })
-                
-            }else if (direction == ItemTouchHelper.RIGHT){
-                database.child("Users").child(firebaseAuthCUser).child("Notes").addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        snapshot.children.forEach {
-                            val values = it.getValue(Notes::class.java)
-                            if (values != null) {
-                                if (values.task_name == noteName.toString()) {
-                                    val key = it.key
-                                    if (values.status == "INCOMPLETED") {
-                                        database.child("Users").child(firebaseAuthCUser).child("Notes").child(key.toString()).child("status").setValue("COMPLETED")
-                                        adapter.notifyDataSetChanged()
-                                        task_name_tv.paintFlags = task_name_tv.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                                    } else if (values.status == "COMPLETED") {
-                                        database.child("Users").child(firebaseAuthCUser).child("Notes").child(key.toString()).child("status").setValue("INCOMPLETED")
-                                        adapter.notifyDataSetChanged()
-                                        task_name_tv.paintFlags = 0
+                database.child("Users").child(firebaseAuthCUser).child("Notes").addListenerForSingleValueEvent(
+                    object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            snapshot.children.forEach {
+                                val values = it.getValue(Notes::class.java)
+                                if (values != null) {
+                                    if (values.task_name == noteName.toString()) {
+                                        val key = it.key
+                                        val deletedData = it.getValue(Notes::class.java)
+                                        database.child("Users").child(firebaseAuthCUser)
+                                            .child("Notes").child(
+                                            key.toString()
+                                        ).removeValue()
+                                        Snackbar.make(
+                                            findViewById(R.id.all_tasks_recycler),
+                                            noteName,
+                                            Snackbar.LENGTH_LONG
+                                        ).setAction("Undo", View.OnClickListener {
+                                            database.child("Users").child(firebaseAuthCUser)
+                                                .child("Notes").child(
+                                                    deletedData?.key.toString()
+                                                ).setValue(deletedData)
+                                            if (deletedData != null) {
+                                                adapter.add(
+                                                    position, tasks_viewholder(
+                                                        adapter,
+                                                        deletedData
+                                                    )
+                                                )
+                                            }
+                                        }).show()
                                     }
                                 }
                             }
                         }
-                    }
 
-                    override fun onCancelled(error: DatabaseError) {
-                        //TODO("Not yet implemented")
-                    }
+                        override fun onCancelled(error: DatabaseError) {
+                            //TODO("Not yet implemented")
+                        }
 
-                })
+                    })
+                
+            }else if (direction == ItemTouchHelper.RIGHT){
+                database.child("Users").child(firebaseAuthCUser).child("Notes").addListenerForSingleValueEvent(
+                    object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            snapshot.children.forEach {
+                                val values = it.getValue(Notes::class.java)
+                                if (values != null) {
+                                    if (values.task_name == noteName.toString()) {
+                                        val key = it.key
+                                        if (values.status == "INCOMPLETED") {
+                                            database.child("Users").child(firebaseAuthCUser)
+                                                .child("Notes").child(
+                                                key.toString()
+                                            ).child("status").setValue("COMPLETED")
+                                            adapter.notifyDataSetChanged()
+                                            task_name_tv.paintFlags =
+                                                task_name_tv.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                                        } else if (values.status == "COMPLETED") {
+                                            database.child("Users").child(firebaseAuthCUser)
+                                                .child("Notes").child(
+                                                key.toString()
+                                            ).child("status").setValue("INCOMPLETED")
+                                            adapter.notifyDataSetChanged()
+                                            task_name_tv.paintFlags = 0
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            //TODO("Not yet implemented")
+                        }
+
+                    })
             }
         }
 
-        override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
-            RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(this@HomepageActivity,R.color.red_light))
+        override fun onChildDraw(
+            c: Canvas,
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            dX: Float,
+            dY: Float,
+            actionState: Int,
+            isCurrentlyActive: Boolean
+        ) {
+            RecyclerViewSwipeDecorator.Builder(
+                c,
+                recyclerView,
+                viewHolder,
+                dX,
+                dY,
+                actionState,
+                isCurrentlyActive
+            )
+                    .addSwipeLeftBackgroundColor(
+                        ContextCompat.getColor(
+                            this@HomepageActivity,
+                            R.color.red_light
+                        )
+                    )
                     .addSwipeLeftActionIcon(R.drawable.ic_delete)
                     .addSwipeLeftLabel("Delete")
-                    .addSwipeRightBackgroundColor(ContextCompat.getColor(this@HomepageActivity,R.color.green))
+                    .addSwipeRightBackgroundColor(
+                        ContextCompat.getColor(
+                            this@HomepageActivity,
+                            R.color.green
+                        )
+                    )
                     .addSwipeRightActionIcon(R.drawable.ic_baseline_done_24)
                     .addSwipeRightLabel("Mark As Read")
                     .create()
@@ -188,39 +249,39 @@ class HomepageActivity : AppCompatActivity() {
     }
     private fun fetchNotes(){
         database.child("Users").child(firebaseAuthCUser).child("Notes").addListenerForSingleValueEvent(
-                object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
+            object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
 
 
-                        snapshot.children.forEach {
-                            val name_display = it.getValue(Notes::class.java)
-                            if (name_display != null) {
-                                adapter.add(tasks_viewholder(adapter, name_display))
-
-                            }
-                        }
-                        findViewById<RecyclerView>(R.id.all_tasks_recycler).adapter = adapter
-
-
-                        adapter.setOnItemClickListener { item, view ->
-                            val userData = item as tasks_viewholder
-                            val intent = Intent(view.context, TaskDetailsActivity::class.java)
-                            intent.putExtra(TASK_NAME_KEY, userData.user.task_name)
-                            intent.putExtra(TASK_HOUR_KEY, userData.user.hour)
-                            intent.putExtra(TASK_MINUTE_KEY, userData.user.min)
-                            intent.putExtra(TASK_DATE_KEY, userData.user.date)
-                            intent.putExtra(TASK_NOTE_KEY, userData.user.note)
-                            intent.putExtra(TASK_COMPLETED_KEY, userData.user.status)
-                            intent.putExtra(TASK_UID_KEY, userData.user.key)
-                            startActivity(intent)
+                    snapshot.children.forEach {
+                        val name_display = it.getValue(Notes::class.java)
+                        if (name_display != null) {
+                            adapter.add(tasks_viewholder(adapter, name_display))
 
                         }
                     }
+                    findViewById<RecyclerView>(R.id.all_tasks_recycler).adapter = adapter
 
-                    override fun onCancelled(error: DatabaseError) {
-                        //TODO("Not yet implemented")
+
+                    adapter.setOnItemClickListener { item, view ->
+                        val userData = item as tasks_viewholder
+                        val intent = Intent(view.context, TaskDetailsActivity::class.java)
+                        intent.putExtra(TASK_NAME_KEY, userData.user.task_name)
+                        intent.putExtra(TASK_HOUR_KEY, userData.user.hour)
+                        intent.putExtra(TASK_MINUTE_KEY, userData.user.min)
+                        intent.putExtra(TASK_DATE_KEY, userData.user.date)
+                        intent.putExtra(TASK_NOTE_KEY, userData.user.note)
+                        intent.putExtra(TASK_COMPLETED_KEY, userData.user.status)
+                        intent.putExtra(TASK_UID_KEY, userData.user.key)
+                        startActivity(intent)
+
                     }
-                })
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    //TODO("Not yet implemented")
+                }
+            })
     }
 
 }
